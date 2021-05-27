20a27afd28730f6befe87631eb53ae13484d4e1e
Huddle OnPC <huddleonpc@gmail.com>
2021-05-26 17:58:14 -0700

    Update monke.py

--- monke.py
+++ monke.py
@@ -1,6 +1,6 @@
 print("Starting")
 print("Creating files")
-with open('monke.txt', 'w') as f:
+with open('monke.html', 'w') as f:
     for x in range(200000000):
         f.write('monke ')
 print("finished monke")
